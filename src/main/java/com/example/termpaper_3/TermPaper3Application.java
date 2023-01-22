a23d82a6df3f06483fe8df6dfb10d5a5274c40c9
AllaKulchitskaya <alla070@list.ru>
2023-01-22 14:00:33 +0300

    Initial commit

---
+++ src/main/java/com/example/termpaper_3/TermPaper3Application.java
@@ -0,0 +1,13 @@
+package com.example.termpaper_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermPaper3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TermPaper3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/termpaper_3/TermPaper3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.termpaper_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermPaper3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
